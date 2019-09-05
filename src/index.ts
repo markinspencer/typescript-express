682a0393cc3746a3a9823909da42678d46fa44d1
Spencer Markin <smarkin@questica.com>
2019-09-05 15:22:09 -0400

    login logic

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import bodyParser from "body-parser";
 import express from "express";
+
 import { router } from "./routes/loginRoutes";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(3000, () => {
