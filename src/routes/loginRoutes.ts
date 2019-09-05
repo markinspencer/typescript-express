4cb8123ce1198afa3282dd934b4576a69c3ab317
Spencer Markin <smarkin@questica.com>
2019-09-05 15:43:40 -0400

    add RequestWithBody interface

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,5 +1,8 @@
 import { Router, Request, Response } from "express";
 
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
 const router = Router();
 
 router.get("/login", (req: Request, res: Response) => {
@@ -18,11 +21,13 @@ router.get("/login", (req: Request, res: Response) => {
   `);
 });
 
-router.post("/login", (req: Request, res: Response) => {
+router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
-  res.send(`
-    ${email + password}
-  `);
+  if (email) {
+    res.send(email.toUpperCase());
+  } else {
+    res.send("you must enter an email!");
+  }
 });
 
 export { router };
